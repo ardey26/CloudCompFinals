88d518d9040b27905ccffd55563e49632b8bcb0f
ardey26 <andorekun26@gmail.com>
2023-11-19 14:48:10 +0800

    Added todo list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [item, setItem] = useState("Text!")
 
   return (
     <>      
-        <div className='font-bold'> Hello, world!</div>
+      <div id="todo-container">
+        <h2>To-Do List</h2>
+        <input type="text" id="task-input" placeholder="Add a new task..." value={item}/>
+        <ul id="task-list"></ul>
+        <button onclick="addTask()">Add Task</button>
+      </div>
     </>
   )
 }
