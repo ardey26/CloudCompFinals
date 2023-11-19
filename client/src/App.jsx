c4d1fea527d08284ccda351579e1d2f945d1be57
ardey26 <andorekun26@gmail.com>
2023-11-19 14:56:04 +0800

    Fixed styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,23 @@ import './App.css'
 function App() {
   const [item, setItem] = useState("Text!")
 
+  function handleChange(e) {
+    setItem(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+  }
   return (
-    <>      
-      <div id="todo-container">
-        <h2>To-Do List</h2>
-        <input type="text" id="task-input" placeholder="Add a new task..." value={item}/>
+    <div class="grid grid-cols-5 gap-4">      
+      <div className="border border-solid border-black p-4 inline-block">
+        <h2 className='font-bold text-left'>Upload Picture</h2>
+        <input className='border border-solid border-black rounded-lg' type="text" id="task-input" placeholder="Add a new task..." value={item} />
         <ul id="task-list"></ul>
         <button onclick="addTask()">Add Task</button>
       </div>
-    </>
+    </div>
   )
 }
 
